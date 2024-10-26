d4b6755f20fac1b3d13bf38973a353e0846db854
Anderson Sanches <andycds@gmail.com>
2024-10-25 21:40:52 -0300

    observações

--- observacoes/index.js
+++ observacoes/index.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const app = express()
 app.use(express.json())
+const { v4: uuidv4 } = require('uuid');
+
+
+const observacoesPorLembreteId = {}
 
 app.post('/lembretes/:id/observacoes', (req, res) => {
-
+    const idObs = uuidv4();
+    const { texto } = req.body;
+    //req.params dá acesso a lista de parâmetros da URL
+    const observacoesDoLembrete = observacoesPorLembreteId[req.params.id] || [];
+    observacoesDoLembrete.push({ id: idObs, texto });
+    observacoesPorLembreteId[req.params.id] = observacoesDoLembrete;
+    res.status(201).send(observacoesDoLembrete);
 })
 
 app.get('/lembretes/:id/observacoes', (req, res) => {
-
+    res.send(observacoesPorLembreteId[req.params.id] || []);
 })
 
 app.listen(5000, () => {
