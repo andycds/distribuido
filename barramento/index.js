a8bcaad7cd4a7b0013d5b0935e30a91d4aac90d2
Anderson Sanches <andycds@gmail.com>
2024-11-22 19:49:33 -0300

    Microsserviço de consultas

--- barramento/index.js
+++ barramento/index.js
@@ -9,9 +9,11 @@ app.post('/eventos', (req, res) => {
     axios.post('http://localhost:4000/eventos', evento);
     // envia o evento para o microsserviço de obvervações
     axios.post('http://localhost:5000/eventos', evento);
+    // envia o evento para o microsserviço de consultas
+    axios.post('http://localhost:6000/eventos', evento);
     res.send({ msg: "ok" });
 });
 
 app.listen(10000, () => {
-    console.log('Barramento de eventos. Porta');
+    console.log('Barramento de eventos. Porta 10000.');
 });
